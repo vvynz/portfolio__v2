6571e612389d3f8d45483603c47b3b0b25f29706
Vivian Zhang <vvynz12@gmail.com>
2022-06-17 19:38:06 -0400

    Create a state for my title and create a typewriter effect

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+const intro = "front-end developer.";
+
 export default function Header() {
-  return ("HELLO 🧁");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTitle(intro.slice(0, title.length + 1))
+    }, 100)
+
+    return () => clearTimeout(timeout);
+  }, [title])
+
+  return (
+    <h1>Hi 👋🏻 I'm Vivian!
+      <span> I'm a {title}</span>
+    </h1>
+  );
 }
