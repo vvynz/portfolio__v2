2022fe191adfb8f203c9089a30ff232df1690135
Vivian Zhang <vvynz12@gmail.com>
2022-06-21 19:02:37 -0400

    Add minor styling to Skills list

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -6,31 +6,40 @@ import bootstrap from "../assets/icons/bootstrap.svg";
 import js from "../assets/icons/js.svg";
 import node from "../assets/icons/nodejs.svg";
 
+import "../Styles/Skills.scss";
+
 export default function Skills() {
   return (
     <section>
       <h3>My Toolkit</h3>
-      <ul>
+      <ul className="skills">
         <li>
           <img src={htmllogo} alt="html-logo" width={75} />
+          HTML
         </li>
         <li>
           <img src={csslogo} alt="css-logo" width={75} />
+          CSS
         </li>
         <li>
           <img src={js} alt="js-logo" width={75} />
+          Javascript
         </li>
         <li>
           <img src={node} alt="node-logo" width={75} />
+          Node.js
         </li>
         <li>
           <img src={bootstrap} alt="bootstrap-logo" width={75} />
-        </li>
-        <li>
-          <img src={reactlogo} alt="react-logo" width={75} />
+          Bootstrap
         </li>
         <li>
           <img src={github} alt="github-logo" width={75} />
+          Github
+        </li>
+        <li>
+          <img src={reactlogo} alt="react-logo" width={75} />
+          React
         </li>
       </ul>
 
