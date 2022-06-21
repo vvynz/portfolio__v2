a28baca711243038fc4947ab51c5bf904dc4d1a7
Vivian Zhang <vvynz12@gmail.com>
2022-06-21 17:56:36 -0400

    Create assets and icon folder and create a Skills component

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,16 @@
+import reactlogo from "../assets/icons/react.gif";
+
+export default function Skills() {
+  return (
+    <section>
+      <h3>My Toolkit</h3>
+      <ul>
+        <li>
+          <img src={reactlogo} alt="react-logo" />
+        </li>
+      </ul>
+
+
+    </section>
+  )
+}
