aa534948a8c7c0302ce27be51891d3509cb25229
Vivian Zhang <vvynz12@gmail.com>
2022-06-21 17:32:56 -0400

    Add a paragraph of About me

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,6 +2,9 @@ export default function About() {
   return (
     <section>
       <h3>About</h3>
+      <p>
+        Hello and welcome! I began my journey during the Spring of 2021 where one day I discovered an online coding workshops targeted at women. I had completed a couple of coding workshops but felt like there were still gaps in my knowledge. And I decided that I wanted to purse a formal education in web development fulltime at Lighthouse Labs at the beginning of this year. I've recently graduated and am now, looking for my first front-end web developer job. I am hoping I can find a team where I can continue to grow and further my abilities as a web developer.
+      </p>
     </section>
   )
 }
